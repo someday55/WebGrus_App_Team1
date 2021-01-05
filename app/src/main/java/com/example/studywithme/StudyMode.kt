733ff9408198e9c130b091d1a0c3ecdd59f0a936
Taeyoung_Yang <0130yang@gmail.com>
2021-01-06 03:44:30 +0900

    test

---
+++ app/src/main/java/com/example/studywithme/StudyMode.kt
@@ -0,0 +1,11 @@
+package com.example.studywithme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudyMode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.study_mode) // 스터디 모드 화면
+    }
+}
