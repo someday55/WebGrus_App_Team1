c7a00562feb91578dfc63984be7537671ba1351c
junhoflow <50655746+junhoflow@users.noreply.github.com>
2021-01-06 20:58:24 +0900

    Just Studying

--- app/src/main/java/com/example/studywithme/MainActivity.kt
+++ app/src/main/java/com/example/studywithme/MainActivity.kt
@@ -11,12 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_toast.setOnClickListener{
+        loadData()
 
-            iv_profile.setImageResource(R.drawable.robot)
+    }
 
-            Toast.makeText(this@MainActivity, "버튼이 클릭 되었습니다", Toast.LENGTH_LONG).show()
-        }
+    fun loadData() {
+        val pref = getSharedPreferences("pref", 0)
+        et_hello.setText(pref.getString("name", "")) //1번째 인자에는 저장할 당시의 키 값을 작성, 2번째는 데이터가 존재하지 않을 경우 대체 값 작성
+    }
 
+
+    fun saveData() {
+        val pref = getSharedPreferences("pref", 0)
+        val edit = pref.edit() //수정모드
+        edit.putString("name", et_hello.text.toString()) //1번째 인자에는 키 값, 2번째 인자에는 실제 담아둘 값
+        edit.apply()
+    }
+
+
+    override fun onDestroy() { //앱이 종료되는 시점이 다가올 때 호출
+        super.onDestroy()
+
+        saveData()
     }
 }
