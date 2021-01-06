348872886cd6936c6f072a7b6781a580539656fc
junhoflow <50655746+junhoflow@users.noreply.github.com>
2021-01-06 17:55:18 +0900

    Just Studying

--- app/src/main/java/com/example/studywithme/MainActivity.kt
+++ app/src/main/java/com/example/studywithme/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.studywithme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //앱이 최초 실행되면 이곳을 수행한다.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_getText.setOnClickListener{
+            var resultText = et_id.text.toString()
+            tv_result.setText(resultText)
+
+
+        }
+
     }
 }
